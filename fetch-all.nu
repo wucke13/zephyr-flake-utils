bb9632831cef9b942885798544457fddb82c504a
wucke13 <wucke13@gmail.com>
2023-05-26 22:28:13 +0200

    initial commit

---
+++ fetch-all.nu
@@ -0,0 +1,57 @@
+module commands {
+  use std 'log debug'
+  use std 'log info'
+  use std 'log warning'
+  use std 'log error'
+  use std 'log critical'
+
+  export def "gh fetch" [
+    owner:string,
+    repo:string,
+    path:string,
+    --token (-t): string,
+   ] {
+    let base_url = "https://api.github.com/repos"
+    mut headers = []
+    let token = "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"
+
+    if ($token != null) {
+      $headers = ($headers | append [ "Authorization" $"token ($token)" ])
+    }
+    let url = $"($base_url)/($owner)/($repo)/($path)"
+    http get --headers $headers $url
+  }
+
+  export def save-zephyr-release-assets [
+    release:string,
+  ] {
+    let release_data = (gh fetch "zephyrproject-rtos" "sdk-ng" $"releases/tags/($release)")
+    let version = ($release_data.tag_name)
+    log info $"fetching hashes for ($version)"
+    let hashes =  ($release_data.assets
+      | where name == "sha256.sum"
+      | get browser_download_url.0
+      | http get $in
+      | from ssv --noheaders
+      | rename sha256 name
+    )
+
+    $release_data.assets
+      | select name browser_download_url
+      | rename name url
+      | join $hashes name name
+      | save $"zephyr-assets/($version).json"
+  }
+
+  export def get-all-zephyr-release-hashes [] {
+    gh fetch "zephyrproject-rtos" "sdk-ng" "releases"
+      | where draft == false and prerelease == false
+      | get tag_name
+      | par-each {|e| save-zephyr-release-assets $e }
+      | ignore
+  }
+}
+
+use commands *
+
+get-all-zephyr-release-hashes
---
+++ flake.nix
@@ -0,0 +1,183 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mach-nix.url = "github:DavHau/mach-nix";
+    zephyr = {
+      url = "github:zephyrproject-rtos/zephyr";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+
+      # not supported by mach-nix
+      # "x86_64-windows"
+    ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.devshell.overlays.default
+              (final: prev: {
+                npmlock2nix = import inputs.npmlock2nix { pkgs = prev; };
+              })
+            ];
+          };
+          mach-nix = inputs.mach-nix.lib.${system};
+
+          # parse a filename string from a zephyr sdk release asset
+          # returns the following attribute set:
+          # { type, hostArch, targetArch }
+          parse = x:
+            let
+              types = [ "hosttools" "toolchain" "zephyr-sdk" ];
+              host-os = [ "darwin" "linux" "windows" ];
+              host-arch = [ "aarch64" "x86_64" ];
+              target-arch = [
+                "aarch64"
+                "arc"
+                "arc64"
+                "arm"
+                "microblazeel"
+                "mips"
+                "nios2"
+                "riscv64"
+                "sparc"
+                "x86_64"
+                "xtensa-espressif_esp32"
+                "xtensa-espressif_esp32s2"
+                "xtensa-espressif_esp32s3"
+                "xtensa-intel_ace15_mtpm"
+                "xtensa-intel_apl_adsp"
+                "xtensa-intel_bdw_adsp"
+                "xtensa-intel_byt_adsp"
+                "xtensa-intel_s1000"
+                "xtensa-intel_tgl_adsp"
+                "xtensa-nxp_imx8m_adsp"
+                "xtensa-nxp_imx_adsp"
+                "xtensa-sample_controller"
+              ];
+              regexVariants = x: "(" + (builtins.concatStringsSep "|" x) + ")";
+              regex = "${regexVariants types}(-[0-9.]+)?_${regexVariants host-os}-${regexVariants host-arch}(_${regexVariants target-arch}[-_]([^.]*))?\.(.*)$";
+              xWithCorrectOsNames = builtins.replaceStrings [ "macos" ] [ "darwin" ] x;
+              matches = builtins.match regex xWithCorrectOsNames;
+            in
+            rec {
+              type = builtins.elemAt matches 0;
+              hostArch = "${builtins.elemAt matches 3}-${builtins.elemAt matches 2}";
+              targetArch = builtins.elemAt matches 5;
+            };
+
+          # build a zephyr related derivation
+          zephyrBuilder = { name, url, sha256, version, ... }: with pkgs; stdenv.mkDerivation {
+            inherit name version;
+            src = fetchurl {
+              inherit url sha256;
+            };
+
+            nativeBuildInputs = [ autoPatchelfHook ];
+            buildInputs = [ stdenv.cc.cc.lib self.packages.${system}.zephyr-python ];
+
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              cp --recursive * $out/
+              cd $out/
+              runHook postInstall
+            '';
+          };
+        in
+        rec {
+          packages = {
+            # a python with all zephyr packages installed
+            zephyr-python = mach-nix.mkPython {
+              python = "python38";
+              requirements = pkgs.lib.concatStringsSep "\n"
+                (builtins.map (x: builtins.readFile x) [
+                  (inputs.zephyr + "/scripts/requirements-base.txt")
+                  (inputs.zephyr + "/scripts/requirements-build-test.txt")
+                  (inputs.zephyr + "/scripts/requirements-doc.txt")
+                  # TODO make this work
+                  # (inputs.zephyr + "/scripts/requirements-run-test.txt")
+                  (inputs.zephyr + "/scripts/requirements-extras.txt")
+                  (inputs.zephyr + "/scripts/requirements-compliance.txt")
+                ]);
+            };
+          } // (
+            let
+              lib = pkgs.lib;
+              inherit (builtins)
+                filter
+                foldl'
+                fromJSON
+                listToAttrs
+                map
+                readDir
+                readFile
+                replaceStrings;
+              inherit (pkgs.lib)
+                filterAttrs
+                flatten
+                mapAttrsToList
+                optionalString
+                removePrefix
+                removeSuffix;
+              removeFileExt = str: foldl' (acc: ext: removeSuffix ".${ext}" acc) str [ "7z" "tar.gz" "tar.xz" ];
+              fileNameToVersion = name: removePrefix "v" (removeSuffix ".json" name);
+              dirEntries = readDir ./zephyr-assets;
+              assetFiles = filterAttrs (n: v: v == "regular") dirEntries;
+              assetFilesList = mapAttrsToList
+                (name: value: {
+                  version = fileNameToVersion name;
+                  assets = filter (x: system == x.meta.hostArch) (map
+                    (x: x // {
+                      meta = parse x.name;
+                    })
+                    (fromJSON (readFile (./zephyr-assets + "/${name}"))));
+                })
+                assetFiles;
+              packagesList = flatten (map
+                (release: map
+                  (file:
+                    let meta = file.meta; in
+                    {
+                      name = "${meta.type}${optionalString (meta.targetArch != null) "-${meta.targetArch}" }-${replaceStrings [ "." ] [ "_" ] release.version}";
+                      value = zephyrBuilder (file // { inherit (release) version; });
+                    })
+                  release.assets)
+                assetFilesList);
+            in
+            listToAttrs packagesList
+          );
+
+          devShell =
+            let
+              sdk = packages.zephyr-sdk-0_16_1_linux-x86_64-0_16_1;
+              toolchain = packages.toolchain_linux-x86_64_arm-zephyr-eabi-0_16_1;
+            in
+            pkgs.mkShell {
+              ZEPHYR_TOOLCHAIN_VARIANT = "zephyr";
+              ZEPHYR_SDK_INSTALL_DIR = sdk;
+              nativeBuildInputs = with pkgs; [
+                sdk
+                toolchain
+                packages.zephyr-python
+                cmake
+                ninja
+                dtc
+              ];
+            };
+        }
+      );
+}
